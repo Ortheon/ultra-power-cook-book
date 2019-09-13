5d5ce4bc81ea84eb5ce84041d481c9a9a214836b
Ortheon <alaricker2@gmail.com>
2019-09-13 22:41:21 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
+import { RecipeInfoComponent } from './recipe/recipe-info/recipe-info.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { EditComponent } from './shopping-list/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    RecipeComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeEditComponent,
+    RecipeInfoComponent,
+    ShoppingListComponent,
+    EditComponent,
   ],
   imports: [
     BrowserModule,
